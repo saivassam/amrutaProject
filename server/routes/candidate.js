b6a02b476d25e914ad54dba12bd3bdfffb89521d
Pavan Jadhav <pavanjadhav45710@gmail.com>
2023-08-25 01:56:53 +0530

    adding verififcation to createCandidate

--- server/routes/candidate.js
+++ server/routes/candidate.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = express.Router();
+const verifyToken = require("../authMiddleware/authMiddleware")
 const {createCandidate,updateCandidate,deleteCandidate,getCandidate,getAllCandidate} = require("../controllers/candidateController")
 
-route.post("/candidateCreate",createCandidate);
+route.post("/candidateCreate",verifyToken,createCandidate);
 route.put("/candidateUpdate/:id",updateCandidate);
 route.delete("/deleteCandidate/:id",deleteCandidate);
 route.get("/getCandidate/:id",getCandidate);
